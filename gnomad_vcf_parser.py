cfa83ec99dea5229fb6f9ba98ab52adb99868824
KK <kkrysiak@genome.wustl.edu>
2017-10-20 13:39:19 -0500

    Commented code to fascilitate readibility

--- gnomad_vcf_parser.py
+++ gnomad_vcf_parser.py
@@ -1,8 +1,3 @@
-## Parses the imported gnomAD VCF file
-## demultiplex alleles
-## extract the allele frequencies
-## left align the reference
-## Export a hash where key is chr,start,ref,var and value is allele freq (allele counts?)
 import vcf
 
 class GnomadVcfParser:
@@ -10,23 +5,35 @@ class GnomadVcfParser:
         self.path_g_vcf = path_g_vcf
 
     def parse_vcf(self):
+        # create a reader object
         vcf_reader = vcf.Reader(open(self.path_g_vcf, "rb"))
         g_hash = {}
+        # reads in the vcf line by line
         for line in vcf_reader:
+            # Simultaneously iterates over the three lists (alt allele, alt allele freq, alt allele count)
+            # For each iteration i (allele), gets the values at position i (all values for that allele)
             for alt, af, ac in zip(line.ALT, line.INFO['AF'], line.INFO['AC']):
+                # Uses class method to left align (normalize) the position, ref, alt alleles for single allele representation
                 new_pos, new_ref, new_alt = GnomadVcfParser.get_minimal_representation(line.POS,line.REF,str(alt))
+                # Uses the chr, start, ref, alt as a hash key to provide INFO field information
                 key = "_".join([str(line.CHROM),str(new_pos),new_ref,new_alt])
+                # Creates a hash of hashes to provide multiple INFO fields per alternate allele
                 g_hash[key] = {}
                 g_hash[key]['af'] = af
                 g_hash[key]['ac'] = ac
+                # Only one allele number is provided for each position, regardless of # of alternative alleles
                 g_hash[key]['an'] = line.INFO['AN']
         return(g_hash)
 
+    # Removes extra bases added to allow for collapsing of nearby indels into multi-allele VCF representation
+    # Provides new start, ref, alt
+    # Stolen from http://www.cureffi.org/2014/04/24/converting-genetic-variants-to-their-minimal-representation/
     @classmethod
     def get_minimal_representation(cls, pos, ref, alt):
         # If it's a simple SNV, don't remap anything
         if len(ref) == 1 and len(alt) == 1:
             return pos, ref, alt
+        # Handle indels
         else:
             # strip off identical suffixes
             while (alt[-1] == ref[-1] and min(len(alt), len(ref)) > 1):
