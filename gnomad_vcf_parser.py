9517487ed61cbd7df6a4a8a47cf40d46b94ee53f
KK <kkrysiak@genome.wustl.edu>
2017-10-20 10:41:15 -0500

    adding test file to parser

--- gnomad_vcf_parser.py
+++ gnomad_vcf_parser.py
@@ -3,13 +3,18 @@
 ## extract the allele frequencies
 ## left align the reference
 ## Export a hash where key is chr,start,ref,var and value is allele freq (allele counts?)
-
 import vcf
 
 class GnomadVcfParser:
     def __init__(self, path_g_vcf):
         self.path_g_vcf = path_g_vcf
     def parse_vcf(self):
-        vcf_reader = vcf.Reader(open(self.path_g_vcf))
+        vcf_reader = vcf.Reader(open(self.path_g_vcf, "rb"))
         for line in vcf_reader:
-            
+            import pdb
+            pdb.set_trace()
+            pass
+
+
+parser = GnomadVcfParser("/Users/kkrysiak/git/gnomad_annotation/test.gnomad.vcf.gz")
+parsed_vcf = parser.parse_vcf()
