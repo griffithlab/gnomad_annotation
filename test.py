d2171e10c64604c0fddc40ce2d9d9e43061b85b5
Kilannin Krysiak <kkrysiak@genome.wustl.edu>
2017-10-25 12:54:41 -0500

    Rekeyed hash with mixed up input files

--- test.py
+++ test.py
@@ -40,30 +40,32 @@ args = input_parser.parse_args()
 
 
 # Choose the correct gnomAD version
-#vcf_key="_".join([args.build, args.gnomad_version, args.gnomad_type])
+vcf_key="_".join([args.build, args.gnomad_version, args.gnomad_type])
 # Hash of versions and their VCF locations
-#vcf_loc = {
-#    'GRCH37_2.0.1_exomes' : '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/2dd4b53431674786b760adad60a29273/gnomad.exomes.r2.0.1.sites.GRCh38.noVEP.vcf.gz',
-#    'GRCH37_2.0.1_genomes': '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/2dd4b53431674786b760adad60a29273/gnomad.genomes.r2.0.1.sites.GRCh38.noVEP.vcf.gz',
-#    'GRCH38_2.0.1_exomes': '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/e6fedd72a7c046a895e2647f06625171/gnomad.exomes.r2.0.1.sites.noVEP.vcf.gz',
-#    'GRCH38_2.0.1_genomes': '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/e6fedd72a7c046a895e2647f06625171/gnomad.genomes.r2.0.1.sites.noVEP.vcf.gz',
-#}
+vcf_loc = {
+    'GRCH38_2.0.1_exomes' : '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/2dd4b53431674786b760adad60a29273/gnomad.exomes.r2.0.1.sites.GRCh38.noVEP.vcf.gz',
+    'GRCH38_2.0.1_genomes': '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/2dd4b53431674786b760adad60a29273/gnomad.genomes.r2.0.1.sites.GRCh38.noVEP.vcf.gz',
+    'GRCH37_2.0.1_exomes': '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/e6fedd72a7c046a895e2647f06625171/gnomad.exomes.r2.0.1.sites.noVEP.vcf.gz',
+    'GRCH37_2.0.1_genomes': '/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/e6fedd72a7c046a895e2647f06625171/gnomad.genomes.r2.0.1.sites.noVEP.vcf.gz'
+}
 
-#parser = GnomadVcfParser(vcf_loc[vcf_key])
-parser = GnomadVcfParser("/Users/kkrysiak/git/gnomad_annotation/test.gnomad.vcf.gz")
-#parser = GnomadVcfParser("/gscmnt/gc2560/core/model_data/genome-db-ensembl-gnomad/e6fedd72a7c046a895e2647f06625171/gnomad.exomes.r2.0.1.sites.noVEP.vcf.gz")
+print(vcf_key)
+print("\nUsing gnomAD input: ", vcf_loc[vcf_key])
+
+#parser = GnomadVcfParser("/gscuser/kkrysiak/git/gnomad_annotation/test.gnomad.vcf.gz")
+#parser = GnomadVcfParser("/Users/kkrysiak/git/gnomad_annotation/test.gnomad.vcf.gz")
+parser = GnomadVcfParser(vcf_loc[vcf_key])
 parsed_vcf = parser.parse_vcf()
-print(parsed_vcf)
+#print(parsed_vcf)
 
-# print(parsed_vcf['1_13538_G_A'])
-# print(parsed_vcf['1_13538_G_T'])
-
-#with open("test_MGIannotation.tsv", "r") as mgi_tsv, open("outfile.tsv", "w") as outfile:
-#with open(input_parser.parse_args('input_file')) as mgi_tsv, open(input_parser.parse_args('output_file')) as outfile:
+# Read in variant file and print new annotated file
 with open(args.input_file.name, "r") as mgi_tsv, open(args.output_file.name, "w") as outfile:
     mgi_tsv_reader = csv.DictReader(mgi_tsv, delimiter="\t")
     header = mgi_tsv_reader.fieldnames
-    header_new = header + ["gnomAD_AC","gnomAD_AN","gnomAD_AF"]
+    ac_head = "gnomAD_AC_"+args.gnomad_type
+    an_head = "gnomAD_AN_"+args.gnomad_type
+    af_head = "gnomAD_AF_"+args.gnomad_type
+    header_new = header + [ac_head,an_head,af_head]
     mgi_tsv_writer = csv.DictWriter(outfile, fieldnames=header_new, delimiter="\t")
     mgi_tsv_writer.writeheader()
     for line in mgi_tsv_reader:
@@ -72,13 +74,13 @@ with open(args.input_file.name, "r") as mgi_tsv, open(args.output_file.name, "w"
             mgi_key = mgi_key.replace("-","0")
         if mgi_key in parsed_vcf:
             new_line = line.copy()
-            new_line["gnomAD_AC"] = parsed_vcf[mgi_key]['ac']
-            new_line["gnomAD_AN"] = parsed_vcf[mgi_key]['an']
-            new_line["gnomAD_AF"] = parsed_vcf[mgi_key]['af']
+            new_line[ac_head] = parsed_vcf[mgi_key]['ac']
+            new_line[an_head] = parsed_vcf[mgi_key]['an']
+            new_line[af_head] = parsed_vcf[mgi_key]['af']
             mgi_tsv_writer.writerow(new_line)
         else:
             new_line = line.copy()
-            new_line["gnomAD_AC"] = "NA"
-            new_line["gnomAD_AN"] = "NA"
-            new_line["gnomAD_AF"] = "NA"
+            new_line[ac_head] = "NA"
+            new_line[an_head] = "NA"
+            new_line[af_head] = "NA"
             mgi_tsv_writer.writerow(new_line)
