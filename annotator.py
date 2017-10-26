ab696e340b930516bc3c9679872e7d8924dbd3f9
Kelsy Cotto <kelsy.cotto@wustl.edu>
2017-10-25 21:29:50 -0500

    print statements

--- annotator.py
+++ annotator.py
@@ -8,14 +8,18 @@ def read_key_list(filename):
     print('Reading key list from {}...'.format(filename))
     with open(filename) as tsvfile:
         reader = csv.reader(tsvfile, delimiter='\t')
-        for line_number, line in enumerate(reader):
+        line_count = 0
+        for line in reader:
+            line_count += 1
+            if line_count % 1000000 == 0:
+                print('Read in {} lines from dictionary'.format(line_count))
             gnomad_key = line[0]
             g_hash[gnomad_key] = {}
             g_hash[gnomad_key]['af'] = line[1]
             g_hash[gnomad_key]['ac'] = line[2]
             # Only one allele number is provided for each position, regardless of # of alternative alleles
             g_hash[gnomad_key]['an'] = line[3]
-    print('Done')
+    print('Done reading in key list')
     return g_hash
 
 
@@ -27,13 +31,20 @@ if not os.path.isfile('gnomad_exome_37_dict.tsv'):
 else:
     dict = read_key_list('gnomad_exome_37_dict.tsv')
 
-with open("/gscmnt/gc2547/griffithlab/kkrysiak/lymphoma_group2/filter_variants/756d8642be0a488ca8dcb424d5769ae2/all_variants_whitelist.756d8642be0a488ca8dcb424d5769ae2.chr.coverage.trv.noerror.tsv", "r") as mgi_tsv, open("outfile.tsv", "w") as outfile:
+inputfile = '/gscmnt/gc2547/griffithlab/kkrysiak/lymphoma_group2/filter_variants/756d8642be0a488ca8dcb424d5769ae2/all_variants_whitelist.756d8642be0a488ca8dcb424d5769ae2.chr.coverage.trv.noerror.tsv'
+
+with open(inputfile, "r") as mgi_tsv, open("outfile.tsv", "w") as outfile:
+    print('Beginning comparison')
     mgi_tsv_reader = csv.DictReader(mgi_tsv, delimiter="\t")
     header = mgi_tsv_reader.fieldnames
     header_new = header + ["gnomAD_AC","gnomAD_AN","gnomAD_AF"]
     mgi_tsv_writer = csv.DictWriter(outfile, fieldnames=header_new, delimiter="\t")
     mgi_tsv_writer.writeheader()
+    line_count = 0
     for line in mgi_tsv_reader:
+        line_count += 1
+        if line_count %50000 == 0:
+            print('Processing line {} from inputfile'.format(line_count))
         mgi_key = "_".join([str(line["chromosome_name"]),str(line["start"]),line["reference"],line["variant"]])
         if mgi_key not in dict:
             mgi_key = mgi_key.replace("-","0")
