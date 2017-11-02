6de39f6a541fd4fd88401b83d08ab2c063a17ea2
Kelsy Cotto <kelsy.cotto@wustl.edu>
2017-11-02 15:57:44 -0500

    remove code that was creating dummy file

--- annotator.py
+++ annotator.py
@@ -13,12 +13,10 @@ input_parser.add_argument(
 )
 input_parser.add_argument(
     'input_file',
-    type=argparse.FileType('r'),
     help="5 column, 1-based, tab-separated input file with header to be annotated with gnomAD allele frequencies.",
 )
 input_parser.add_argument(
     'output_file_prefix',
-    type=argparse.FileType('w'),
     help="Final, gnomAD annotated output file location and name without file extension.",
 )
 input_parser.add_argument(
@@ -58,9 +56,8 @@ vcf_loc = {
 
 print("\nUsing gnomAD input: ", vcf_loc[vcf_key])
 
-if args.cutoff != None:
+if args.cutoff is not None:
     print("Using allele frequency cutoff: ", args.cutoff)
-
 
 
 # Read in variant file and print new annotated file
@@ -75,15 +72,15 @@ def annotate(**kwargs):
         ac_head = "gnomAD_AC_"+args.gnomad_type
         an_head = "gnomAD_AN_"+args.gnomad_type
         af_head = "gnomAD_AF_"+args.gnomad_type
-        if args.add_allele_count == True:
-            header_new = header + [ac_head,an_head,af_head]
+        if args.add_allele_count is True:
+            header_new = header + [ac_head, an_head, af_head]
         else:
             header_new = header + [af_head]
-        mgi_tsv_writer = csv.DictWriter(outfile, fieldnames=header_new, delimiter="\t",extrasaction='ignore')
+        mgi_tsv_writer = csv.DictWriter(outfile, fieldnames=header_new, delimiter="\t", extrasaction='ignore')
         mgi_tsv_writer.writeheader()
-        if args.cutoff != None:
+        if args.cutoff is not None:
             fail_file = open(file_fail_out, "w")
-            mgi_tsv_fail_writer = csv.DictWriter(fail_file, fieldnames=header_new, delimiter="\t",extrasaction='ignore')
+            mgi_tsv_fail_writer = csv.DictWriter(fail_file, fieldnames=header_new, delimiter="\t", extrasaction='ignore')
             mgi_tsv_fail_writer.writeheader()
             fail_counter = 0
             pass_counter = 1
@@ -93,19 +90,18 @@ def annotate(**kwargs):
         for line in mgi_tsv_reader:
             line_count += 1
             if line_count % 50000 == 0:
-                print('Processing line {} from inputfile'.format(line_count))
+                print('Processing line {} from input file'.format(line_count))
             mgi_key = "_".join([str(line["chromosome_name"]), str(line["start"]), line["reference"], line["variant"]])
             if mgi_key not in gnomad_annotations:
                 mgi_key = mgi_key.replace("-", "0")
             if mgi_key in gnomad_annotations:
                 gnomad_record = gnomad_annotations[mgi_key]
-                # print(gnomad_record)
                 new_line = line.copy()
                 new_line[ac_head] = gnomad_record[0][1]
                 new_line[an_head] = gnomad_record[0][2]
                 new_line[af_head] = gnomad_record[0][0]
                 match_counter += 1
-                if args.cutoff == None:
+                if args.cutoff is None:
                     mgi_tsv_writer.writerow(new_line)
                 else:
                     if float(new_line[af_head]) >= args.cutoff:
@@ -124,20 +120,20 @@ def annotate(**kwargs):
         print("\nTotal variants processed: ", line_count)
         print("Total variants not found in gnomAD", args.gnomad_type, ": ", not_found_counter)
         print("Total variants matched gnomAD", args.gnomad_type, ": ", match_counter)
-        if args.cutoff != None:
+        if args.cutoff is not None:
             print("Total variants failed allele frequency cutoff of", args.cutoff, ": ", fail_counter)
             print("Total variants passed allele frequency cutoff of", args.cutoff, ": ", pass_counter)
-            fail_file.close()
+            # fail_file.close()
         print("\n")
 
 records = pickle.load(open(vcf_loc[vcf_key], 'rb'))
 print('Finished reading records')
 
-# Get the correct file suffix based on whether filtering is happening or not
-if args.cutoff == None:
-    file_out = str(args.output_file_prefix.name + '.tsv')
-    annotate(mutation_filename=args.input_file.name, file_out=file_out, gnomad_annotations=records)
+#Get the correct file suffix based on whether filtering is happening or not
+if args.cutoff is None:
+    file_out = str(args.output_file_prefix) + '.tsv'
+    annotate(mutation_filename=args.input_file, file_out=file_out, gnomad_annotations=records)
 else:
-    file_out = str(args.output_file_prefix.name + 'pass.tsv')
-    file_fail_out = str(args.output_file_prefix.name + 'fail.tsv')
-    annotate(mutation_filename=args.input_file.name, file_out=file_out, gnomad_annotations=records, file_fail_out=file_fail_out)
+    file_out = str(args.output_file_prefix) + 'pass.tsv'
+    file_fail_out = str(args.output_file_prefix) + 'fail.tsv'
+    annotate(mutation_filename=args.input_file, file_out=file_out, gnomad_annotations=records, file_fail_out=file_fail_out)
